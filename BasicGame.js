c6997bb2b93148ef3540b29f22d41f6a49352427
Daisho <daishokomiyama@gmail.com>
2018-01-26 23:16:26 -0500

    Added background

--- BasicGame.js
+++ BasicGame.js
@@ -3,34 +3,50 @@
 var Game = {
 	canvas: '',
 	canvasContext: '',
-	rectanglePosition: 0
+	backgroundSprite: '',
+	balloonSprite: '',
+	balloonPosition: {
+		x: 0,
+		y: 50
+	}
 };
 
 Game.start = function() {
 	Game.canvas = document.getElementById('myCanvas');
 	Game.canvasContext = Game.canvas.getContext('2d');
-	Game.mainLoop();
+	Game.balloonSprite = new Image();
+	Game.backgroundSprite = new Image();
+	Game.balloonSprite.src = 'spr_balloon.png';
+	Game.backgroundSprite.src = 'spr_background.jpg';
+	window.setTimeout(Game.mainLoop, 500);
 };
 
 document.addEventListener('DOMContentLoaded', Game.start);
 
 Game.update = function() {
 	var d = new Date();
-	Game.rectanglePosition = d.getTime() % Game.canvas.width;
+	Game.balloonPosition.x = d.getTime() % Game.canvas.width;
 };
 
 Game.draw = function() {
-	Game.canvasContext.fillStyle = 'blue';
-	Game.canvasContext.fillRect(Game.rectanglePosition, 100, 50, 50);
+	Game.drawImage(Game.backgroundSprite, {x:0, y:0});
+	Game.drawImage(Game.balloonSprite, Game.balloonPosition);
+};
+
+Game.drawImage = function(sprite, position) {
+	Game.canvasContext.save();
+	Game.canvasContext.translate(position.x, position.y);
+	Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, 0, 0, sprite.width, sprite.height);
+	Game.canvasContext.restore();
 };
 
 Game.clearCanvas = function() {
 	Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
-}
+};
 
 Game.mainLoop = function() {
 	Game.clearCanvas();
 	Game.update();
 	Game.draw();
 	window.setTimeout(Game.mainLoop, 1000/60);
-}
+};
