e371ef0d247fc6d7818a1263e999d5225aa9bb1f
Daisho <daishokomiyama@gmail.com>
2018-01-27 11:14:00 -0500

    Moving balloon on mouse move

--- BasicGame.js
+++ BasicGame.js
@@ -1,56 +1,71 @@
 'use strict';
 
+function handleMouseMove(event) {
+	Game.mousePosition = {
+		x: event.pageX,
+		y: event.pageY
+	};
+}
+
 var Game = {
 	canvas: '',
 	canvasContext: '',
 	backgroundSprite: '',
 	balloonSprite: '',
-	balloonPosition: {
+	mousePosition: {
 		x: 0,
-		y: 50
+		y: 0
 	},
-	backgroundMusic:''
-};
-
-Game.start = function() {
-	Game.canvas = document.getElementById('myCanvas');
-	Game.canvasContext = Game.canvas.getContext('2d');
-	Game.balloonSprite = new Image();
-	Game.backgroundSprite = new Image();
-	Game.backgroundMusic = new Audio();
-	Game.balloonSprite.src = 'spr_balloon.png';
-	Game.backgroundSprite.src = 'spr_background.jpg';
-	Game.backgroundMusic.src = 'snd_music.mp3';
-	Game.backgroundMusic.play();
-	window.setTimeout(Game.mainLoop, 500);
-};
-
-document.addEventListener('DOMContentLoaded', Game.start);
-
-Game.update = function() {
-	var d = new Date();
-	Game.balloonPosition.x = d.getTime() % Game.canvas.width;
-};
-
-Game.draw = function() {
-	Game.drawImage(Game.backgroundSprite, {x:0, y:0});
-	Game.drawImage(Game.balloonSprite, Game.balloonPosition);
-};
-
-Game.drawImage = function(sprite, position) {
-	Game.canvasContext.save();
-	Game.canvasContext.translate(position.x, position.y);
-	Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, 0, 0, sprite.width, sprite.height);
-	Game.canvasContext.restore();
+	balloonOrigin: {
+		x: 0,
+		y: 0
+	}
 };
 
 Game.clearCanvas = function() {
 	Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
 };
 
+Game.drawImage = function(sprite, position, origin) {
+	Game.canvasContext.save();
+	Game.canvasContext.translate(position.x, position.y);
+	Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, -origin.x, -origin.y, sprite.width, sprite.height);
+	Game.canvasContext.restore();
+};
+
+Game.start = function() {
+	Game.canvas = document.getElementById('myCanvas');
+	Game.canvasContext = Game.canvas.getContext('2d');
+	document.onmousemove = handleMouseMove;
+	Game.backgroundSprite = new Image();
+	Game.backgroundSprite.src = 'spr_background.jpg';
+	Game.balloonSprite = new Image();
+	Game.balloonSprite.src = 'spr_balloon.png';
+	window.setTimeout(Game.mainLoop, 500);
+};
+
+document.addEventListener('DOMContentLoaded', Game.start);
+
 Game.mainLoop = function() {
 	Game.clearCanvas();
 	Game.update();
 	Game.draw();
-	window.setTimeout(Game.mainLoop, 1000/60);
+	window.setTimeout(Game.mainLoop, 1000 / 60);
 };
+
+Game.update = function() {
+};
+
+Game.draw = function() {
+	Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 }, { x: 0, y: 0 });
+	Game.balloonOrigin = {
+		x: Game.balloonSprite.width / 2,
+		y: Game.balloonSprite.height / 2
+	};
+	Game.drawImage(Game.balloonSprite, Game.mousePosition, Game.balloonOrigin);
+};
+
+
+
+
+
